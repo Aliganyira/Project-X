75b41229d722ca40e0e0aee36766c428150dc1a9
Kavuma Mark <aliganyirakawemba@gmail.com>
2023-09-28 11:05:41 +0300

    name

--- Test/resources/views/welcome.blade.php
+++ Test/resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-600 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
@@ -34,7 +34,7 @@
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
             
                 <div class="flex justify-center">
-                <h1 class="mt-3 text-xl font-semibold text-gray-200 dark:text-white">My Peter Aliganyira</h1>
+                <h1 class="mt-3 text-xl font-semibold text-black-200 dark:text-white">My name Peter Aliganyira</h1>
                 </div>
 
                 <div class="mt-16">
